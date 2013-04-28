42fbecf9a1fd836c7fee6e8c05c4a883dc1615d8
admin <admin@192.168.2.102>
2013-04-28 16:30:11 -0700

    test

--- src/main/java/com/grayjay/javaUtilities/concurrency/UpgradableLock.java
+++ src/main/java/com/grayjay/javaUtilities/concurrency/UpgradableLock.java
@@ -3,12 +3,14 @@ package concurrency;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.*;
 
+//test
+
 /**
  * A reentrant read-write lock allowing at most one designated upgradable thread
  * that can switch between reading and writing. Other readers can acquire the
  * lock while the thread with the upgradable lock is downgraded. The upgradable
  * thread blocks while upgrading if other threads hold read locks.
- * <p> 
+ * <p>
  * A thread can initially acquire the lock in any of three modes: read,
  * upgradable, and write. A thread acquiring an upgradable lock starts in the
  * downgraded state. All locks and unlocks are nested. This means that a thread
